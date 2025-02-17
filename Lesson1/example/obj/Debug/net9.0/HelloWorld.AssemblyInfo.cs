3b80b57cc80f5846a22387921f5f7a35dd1facf1
Nawa Mumbwe <mumbwe94@gmail.com>
2025-02-17 20:07:32 +0200

    remove weird files

--- Lesson1/example/obj/Debug/net9.0/HelloWorld.AssemblyInfo.cs
+++ Lesson1/example/obj/Debug/net9.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be9b55e9e90c07d1d5d181b17d361defceb1b2ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec95e0762495dc65696fabd3f9db025a0a3fb73a")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
