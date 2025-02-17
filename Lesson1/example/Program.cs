ec95e0762495dc65696fabd3f9db025a0a3fb73a
Nawa Mumbwe <mumbwe94@gmail.com>
2025-02-17 20:06:16 +0200

    lesson 1 done

---
+++ Lesson1/example/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Give me your weight");
+
+var weight = Console.ReadLine();
+
+Console.WriteLine("Give me your heigh");
+
+var height = Console.ReadLine();
+
+Console.WriteLine($"Your weight is {weight}, Your height is {height}");
